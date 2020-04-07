e9c64496fb7075a4ee51d2aa8a6856f4db51909b
Pedro Hübner <pedronhubner@gmail.com>
2020-04-07 20:43:45 -0300

    Adicionando toString para ArrayList de Paciente

--- Codigos/src/com/semanadois/Paciente.java
+++ Codigos/src/com/semanadois/Paciente.java
@@ -1,21 +1,21 @@
 package com.semanadois;
 
-public class Paciente extends Pessoa {
-    private char tipoSanguineo;
+public class Paciente extends Pessoa implements Comparable{
+    private String tipoSanguineo;
     private char resultado;
 
     public Paciente(String nome, int idade, String dataNascimento, double altura, double peso, char sexo,
-                    String endereco, char tipoSanguineo, char resultado){
+                    String endereco, String tipoSanguineo, char resultado){
         super(nome, idade, dataNascimento, altura, peso, sexo, endereco);
         this.tipoSanguineo = tipoSanguineo;
         this.resultado = resultado;
     }
 
-    public char getTipoSanguineo() {
+    public String getTipoSanguineo() {
         return tipoSanguineo;
     }
 
-    public void setTipoSanguineo(char tipoSanguineo) {
+    public void setTipoSanguineo(String tipoSanguineo) {
         this.tipoSanguineo = tipoSanguineo;
     }
 
@@ -29,5 +29,22 @@ public class Paciente extends Pessoa {
 
     public void exibirHistorico(){
         System.out.println("");
+    }
+
+    @Override
+    public String toString() {
+        return getNome() + ": " + getResultado();
+    }
+
+    @Override
+    public int compareTo(Object outro) {
+        Paciente aux = (Paciente)outro;
+        if (this.getIdade() < aux.getIdade()){
+            return -1;
+        }else if (this.getIdade() > aux.getIdade()){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
