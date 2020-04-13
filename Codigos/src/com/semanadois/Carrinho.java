edefa957150ab0a467c67b433b4eea1213948c1d
Pedro Hübner <pedronhubner@gmail.com>
2020-04-13 11:50:08 -0300

    Criando classe Carrinho que herda de Mercado e implementa método abstrato pegarProduto

--- Codigos/src/com/semanadois/Carrinho.java
+++ Codigos/src/com/semanadois/Carrinho.java
@@ -1,4 +1,32 @@
 package com.semanadois;
 
-public class Carrinho {
+public class Carrinho extends  Mercado{
+    private boolean carrinho;
+
+    public Carrinho(boolean carrinho, String produto, double valor, int qntd, int codigo){
+        super(produto, valor, qntd, codigo);
+        this.carrinho = carrinho;
+    }
+
+    public boolean getCarrinho(){
+        return carrinho;
+    }
+
+    public void setCarrinho(boolean carrinho){
+        this.carrinho = carrinho;
+    }
+
+    public void pegarCarrinho(boolean carrinho){
+        if (carrinho = true){
+            System.out.println("Peguei um carrinho");
+        }
+    }
+
+    @Override
+    public void pegarProduto() {
+        //super.pegarProduto();
+        if (Mercado.qntd > 0){
+            --Mercado.qntd;
+        }
+    }
 }
