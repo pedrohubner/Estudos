210bdff4f938331a77ef9369268d50ef006c363a
Pedro Hübner <pedronhubner@gmail.com>
2020-04-13 17:53:39 -0300

    Add quatro novos produtos para cada tipo de produto

--- Codigos/src/com/semanadois/Cidadao.java
+++ Codigos/src/com/semanadois/Cidadao.java
@@ -14,15 +14,48 @@ public class Cidadao {
         Map <Cliente, Mercado> compra = new HashMap<>();
 
         //Stack para uma lista de produtos que vão ser
-        Mercado produto1 = new Mercado("Sabonete", 4.50, 5, 111);
-        Mercado produto2 = new Mercado("Shampoo", 10.00, 1, 222);
-        Mercado produto3 = new Mercado("Desodorante", 7.99, 2, 333);
-        Mercado produto4 = new Mercado("Pão", 3.99, 2, 444);
 
-        produto.push(produto1);
-        produto.push(produto2);
-        produto.push(produto3);
-        produto.push(produto4);
+        //produto sabonete
+        Mercado produto11 = new Mercado("Sabonete Lux", 4.00, 8, 111);
+        Mercado produto12 = new Mercado("Sabonete Rexona", 6.50, 10, 112);
+        Mercado produto13 = new Mercado("Sabonete Panvel", 3.99, 12, 113);
+        Mercado produto14 = new Mercado("Sabonete Glicerina", 5.00, 9, 114);
+        //Produto Shampoo
+        Mercado produto21 = new Mercado("Shampoo Clearmen", 12.15, 20, 221);
+        Mercado produto22 = new Mercado("Shampoo Nivea", 9.50, 16, 222);
+        Mercado produto23 = new Mercado("Shampoo Pantene", 15.49, 15, 223);
+        Mercado produto24 = new Mercado("Shampoo Rexona", 8.99, 8, 224);
+        //Produto desodorante
+        Mercado produto31 = new Mercado("Desodorante Adidas", 7.99, 20, 331);
+        Mercado produto32 = new Mercado("Desodorante Rexona", 5.80, 18, 332);
+        Mercado produto33 = new Mercado("Desodorante Nivea", 6.50, 15, 333);
+        Mercado produto34 = new Mercado("Desodorante Axe", 8.99, 24, 334);
+        //Produto pão
+        Mercado produto41 = new Mercado("Pão Farias", 2.99, 25, 441);
+        Mercado produto42 = new Mercado("Pão Nutrella", 4.50, 18, 442);
+        Mercado produto43 = new Mercado("Pão SevenBoys", 5.00, 17, 443);
+        Mercado produto44 = new Mercado("Pão Bazotti", 3.99, 19, 444);
+
+        //Sabonete
+        produto.push(produto11);
+        produto.push(produto12);
+        produto.push(produto13);
+        produto.push(produto14);
+        //Shampoo
+        produto.push(produto21);
+        produto.push(produto22);
+        produto.push(produto23);
+        produto.push(produto24);
+        //Desodorante
+        produto.push(produto31);
+        produto.push(produto32);
+        produto.push(produto33);
+        produto.push(produto34);
+        //Pãos
+        produto.push(produto41);
+        produto.push(produto42);
+        produto.push(produto43);
+        produto.push(produto44);
 
         System.out.println(produto);
 
@@ -64,10 +97,10 @@ public class Cidadao {
 
         System.out.println(fila);
 
-        compra.put(cliente1, produto2);
-        compra.put(cliente3, produto4);
-        compra.put(cliente2, produto1);
-        compra.put(cliente4, produto3);
+        compra.put(cliente1, produto23);
+        compra.put(cliente3, produto41);
+        compra.put(cliente2, produto12);
+        compra.put(cliente4, produto34);
 
         System.out.println(compra);
 
