97b11b3bc68747a1bda9edfe3dac8f4b6ef60991
Pedro Hübner <pedronhubner@gmail.com>
2020-04-18 11:46:48 -0300

    Criando método comprarProduto: que deve ser corrigido para puxar preço do produto!

--- Codigos/src/com/semanadois/projeto/Cliente.java
+++ Codigos/src/com/semanadois/projeto/Cliente.java
@@ -1,4 +1,34 @@
 package com.semanadois.projeto;
 
 public class Cliente {
+    private String nome;
+    private double qntdDinheiro;
+
+    public Cliente(String nome, double qntdDinheiro){
+        this.nome = nome;
+        this.qntdDinheiro = qntdDinheiro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getQntdDinheiro() {
+        return qntdDinheiro;
+    }
+
+    public void setQntdDinheiro(double qntdDinheiro) {
+        this.qntdDinheiro = qntdDinheiro;
+    }
+
+    public double comprarProduto(double qntdDinheiro){
+        if (this.qntdDinheiro < Farmacia.preco){
+            Exception e;
+        } else System.out.println(qntdDinheiro - Farmacia.preco);
+        return comprarProduto(qntdDinheiro);
+    }
 }
