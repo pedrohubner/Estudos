a22b289a95319909349520f64b945cb83f50bf79
Pedro Hübner <pedronhubner@gmail.com>
2020-04-18 11:47:42 -0300

    Criando método aplicarDesconto

--- Codigos/src/com/semanadois/projeto/Farmacia.java
+++ Codigos/src/com/semanadois/projeto/Farmacia.java
@@ -1,4 +1,32 @@
 package com.semanadois.projeto;
 
 public class Farmacia {
+    private String nomeProduto;
+    static double preco;
+
+    public Farmacia(String nomeProduto, double preco){
+        this.nomeProduto = nomeProduto;
+        this.preco = preco;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double aplicarDesconto(double preco){
+        double desconto = ((preco * 2) /100);
+        return desconto;
+    }
 }
