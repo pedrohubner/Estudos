a07724522e28e78cc87eafdac4141709f7690c03
Pedro Hübner <pedronhubner@gmail.com>
2020-04-19 10:13:34 -0300

    Criando classe main aplicativo e add mensagem inicial

---
+++ Codigos/src/com/semanadois/projeto/Aplicativo.java
@@ -0,0 +1,12 @@
+package com.semanadois.projeto;
+import java.util.Scanner;
+
+public class Aplicativo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("===== Aplicativo Farmácia =====\n Escolhe uma das opções:\n 1 - Exibir dados cliente;\n" +
+                "2 - Exibir lista de produtos;\n 3 - Comprar produto");
+
+    }
+}
