952f5cb785eb558e48acb01d90e2e5d0b2c2fc7f
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-22 09:29:20 -0300

    Implementando método abstrato divisao com expressão lambda

--- Codigos/src/com/semanatres/Aula.java
+++ Codigos/src/com/semanatres/Aula.java
@@ -1,12 +1,34 @@
 package com.semanatres;
 
-public class playground {
-
-//   interface Teste{
-//       default public void soma()
-//   }
-
+public class Aula {
     public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("João", 103);
+
+        Thread t1 = new Thread(() -> System.out.println(a1.getTurma()));
+        t1.start();
+
+
+        Math m1 = new Math() {
+            @Override
+            public void divisao() {
+
+                int a = 6;
+                int b = 3;
+
+                int div = a / b;
+
+                System.out.println(div);
+            }
+        };
+
+        m1.soma();
+
+        m1.divisao();
+
+        Math m2 = () -> System.out.println("Realizando operação de divisão: ");
+
+        m2.divisao();
 
     }
 }
