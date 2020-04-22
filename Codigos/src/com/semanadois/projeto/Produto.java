30b040d1ebdbcf04862ce331aa6546c572193e1c
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-22 11:15:04 -0300

    Add método aplicarDesconto

--- Codigos/src/com/semanadois/projeto/Produto.java
+++ Codigos/src/com/semanadois/projeto/Produto.java
@@ -1,4 +1,39 @@
 package com.semanadois.projeto;
 
 public class Produto {
+
+    private String nomeProduto;
+    private double preco;
+
+    public Produto(String nomeProduto, double preco){
+
+        this.nomeProduto = nomeProduto;
+        this.preco = preco;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double aplicarDesconto(){
+        double desconto = preco/2;
+        return desconto;
+    }
+
+    @Override
+    public String toString() {
+        return " " + nomeProduto + " - " + preco;
+    }
 }
