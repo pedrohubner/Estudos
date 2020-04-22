0f57cc11a03904f08adde7612869e047a7b1c1e7
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-22 09:28:40 -0300

    Interface funcional com método default e um método abstrato

--- Codigos/src/com/semanatres/Math.java
+++ Codigos/src/com/semanatres/Math.java
@@ -1,4 +1,16 @@
 package com.semanatres;
 
 public interface Math {
+
+    default public void soma(){
+        int a = 1;
+        int b = 1;
+
+        int soma = a + b;
+
+        System.out.println(soma);
+    }
+
+    public void divisao();
+
 }
