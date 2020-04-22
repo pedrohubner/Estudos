b78b223e9fa5d38747ddc8ee6c953eb0cb729b0b
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-22 14:57:36 -0300

    Classe criada com o objetivo de estudar Set e seus elementos

--- Codigos/src/com/semanadois/estruturas/JavaSet.java
+++ Codigos/src/com/semanadois/estruturas/JavaSet.java
@@ -1,4 +1,10 @@
 package com.semanadois.estruturas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JavaSet {
+    public static void main(String[] args) {
+        Set<String> nome = new HashSet<>();
+    }
 }
