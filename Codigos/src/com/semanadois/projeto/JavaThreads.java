468a4f71fa230c544285e24350e6515d41a01f7d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-23 11:13:04 -0300

    Criando classe Threads para exemplos de Semaphore e Synchronized

--- Codigos/src/com/semanadois/projeto/JavaThreads.java
+++ Codigos/src/com/semanadois/projeto/JavaThreads.java
@@ -1,4 +1,17 @@
 package com.semanadois.projeto;
 
+import java.util.concurrent.Semaphore;
+
 public class JavaThreads {
+    public static void main(String[] args) {
+
+        Semaphore tes = new Semaphore(2);
+
+        int a = 2;
+        int b = 2;
+
+        int soma = a + b;
+
+//        Thread t1 = new Thread(tes, soma);
+    }
 }
