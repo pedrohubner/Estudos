b7404e321cc4e8f836b21e0319eaca19c7574a44
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 09:39:37 -0300

    Instanciando objetos de JavaThread e AbleToRun e criando thread

--- Codigos/src/com/semanadois/projeto/threads/Teste.java
+++ Codigos/src/com/semanadois/projeto/threads/Teste.java
@@ -3,7 +3,7 @@ package com.semanadois.projeto.threads;
 public class Teste {
     public static void main(String[] args) {
 
-        System.out.println("===Corrida até 9 KM");
+        System.out.println("\n=====Corrida até 9 KM=====\n");
 
         new JavaThread("Lauren").start();
         new JavaThread("Elise").start();
