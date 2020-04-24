c701d4c9b2316a5874396cd658dc52b9d802e8c8
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 09:45:13 -0300

    Tirando try/catch[

--- Codigos/src/com/semanadois/projeto/threads/JavaThread.java
+++ Codigos/src/com/semanadois/projeto/threads/JavaThread.java
@@ -9,10 +9,6 @@ public class JavaThread extends Thread {
     public void run(){
         for (int i = 0; i < 10; i++){
             System.out.println("\n"+getName() + " está em " + i + " km");
-        } try {
-            sleep((long)(Math.random() * 1000));
-        } catch (InterruptedException e){
-            System.out.println("Não começou a corrida");
         }
 
         System.out.println("\nDONE! " + getName());
