3cf6c4df9c908e0eac716eb074e25d7b4d429155
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 12:24:41 -0300

    Criando classe final

--- Codigos/src/com/semanadois/projeto/mutabilidade/JavaImmutable.java
+++ Codigos/src/com/semanadois/projeto/mutabilidade/JavaImmutable.java
@@ -1,4 +1,26 @@
-package com.semanadois;
+package com.semanadois.projeto.mutabilidade;
 
 public class JavaImmutable {
+
+    private final String nome;
+    private final int idade;
+
+    public JavaImmutable(String nome, int idade){
+
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " tem " + idade + " anos";
+    }
 }
