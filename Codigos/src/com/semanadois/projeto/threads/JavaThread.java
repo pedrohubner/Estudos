02ac1655f95a6823e531a146ba10cca7ea79e30c
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 13:09:37 -0300

    Add palavra chave synchronized no método run()

--- Codigos/src/com/semanadois/projeto/threads/JavaThread.java
+++ Codigos/src/com/semanadois/projeto/threads/JavaThread.java
@@ -6,11 +6,11 @@ public class JavaThread extends Thread {
        super(nome);
     }
 
-    public void run(){
+    public synchronized void run(){
         for (int i = 0; i < 10; i++){
             System.out.println("\n"+getName() + " está em " + i + " km");
         }
 
-        System.out.println("\nDONE! " + getName());
+        System.out.println("\n" + getName() + " chegou!");
     }
 }
