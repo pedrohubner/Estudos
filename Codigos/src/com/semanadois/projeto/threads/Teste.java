27741ad0b16a7e4ab68d163774ae35a704a966e5
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 13:11:57 -0300

    Add try/catch para executar thread sincronizada

--- Codigos/src/com/semanadois/projeto/threads/Teste.java
+++ Codigos/src/com/semanadois/projeto/threads/Teste.java
@@ -1,18 +1,29 @@
 package com.semanadois.projeto.threads;
 
 public class Teste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
-        System.out.println("\n=====Corrida até 9 KM=====\n");
+        System.out.println("\n===== Corrida até 9 KM =====\n");
 
-        new JavaThread("Lauren").start();
-        new JavaThread("Elise").start();
+        AbleToRun a = new AbleToRun("Naã");
 
-        AbleToRun e = new AbleToRun("Naã");
+        Thread t1 = new Thread(a);
 
-        Thread t1 = new Thread(e);
+        JavaThread t2 = new JavaThread("Lauren");
+        JavaThread t3 = new JavaThread("Elise");
 
         t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+
+        }catch (InterruptedException e){
+            System.out.println("Corrida interrompida");
+        }
 
     }
 }
