cd05ed464b44a8958425a27a9de372722c16ef3e
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 09:37:27 -0300

    implementando interface Runnable e método run()

--- Codigos/src/com/semanadois/projeto/threads/AbleToRun.java
+++ Codigos/src/com/semanadois/projeto/threads/AbleToRun.java
@@ -1,4 +1,27 @@
 package com.semanadois.projeto.threads;
 
-public class JavaRunnable {
+public class AbleToRun implements Runnable {
+
+    private String nome;
+
+    public AbleToRun(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("\n" + nome + " está em " + i + " km");
+        }{
+            System.out.println("\nDONE! " + nome);
+        }
+    }
 }
