28e6b168aeb43c8fbdf4ca20391fcd6aeda4ceba
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-24 09:38:46 -0300

    Extendendo classe Thread e implementando método run()

--- Codigos/src/com/semanadois/projeto/threads/JavaThread.java
+++ Codigos/src/com/semanadois/projeto/threads/JavaThread.java
@@ -1,4 +1,20 @@
 package com.semanadois.projeto.threads;
 
-public class JavaThread {
+public class JavaThread extends Thread {
+
+    public JavaThread(String nome){
+       super(nome);
+    }
+
+    public void run(){
+        for (int i = 0; i < 10; i++){
+            System.out.println("\n"+getName() + " está em " + i + " km");
+        } try {
+            sleep((long)(Math.random() * 1000));
+        } catch (InterruptedException e){
+            System.out.println("Não começou a corrida");
+        }
+
+        System.out.println("\nDONE! " + getName());
+    }
 }
