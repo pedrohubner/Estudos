f61f034e0abc2b9fe49075970633f04360c98059
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-27 14:55:37 -0300

    Criando exemplo de Stream em cima de uma ArrayList de Alunos

--- Codigos/src/com/playground/exercicios3/LojaVirtual.java
+++ Codigos/src/com/playground/exercicios3/LojaVirtual.java
@@ -1,4 +1,23 @@
 package com.playground.exercicios3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class LojaVirtual {
+    public static void main(String[] args) {
+
+        ArrayList<Celular> produtos = new ArrayList<>();
+
+        produtos.add(new Celular("iPhone 8", 8, 111));
+        produtos.add(new Celular("Samsung JS7", 1, 221));
+        produtos.add(new Celular("Samsung A9", 2, 301));
+
+        /**
+         * Rever aulas de Stream dos 2 cursos
+         * fazer código de novo e corrigir erro do forEach
+         */
+//        produtos.stream().filter(c -> c.getModelo().contains("Samsung"))
+//                .sorted(Comparator.comparingDouble(Celular::getPreco))
+//                .map(celular -> celular.getModelo().toUpperCase()).forEach();
+    }
 }
