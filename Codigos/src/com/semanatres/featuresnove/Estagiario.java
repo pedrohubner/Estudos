fe09b1bb26917da2f8e34b14c14ae9d0e93a2f5b
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-28 13:53:14 -0300

    Implementando interface Pessoa e seus métodos, também criando método expediente

--- Codigos/src/com/semanatres/featuresnove/Estagiario.java
+++ Codigos/src/com/semanatres/featuresnove/Estagiario.java
@@ -1,4 +1,60 @@
 package com.semanatres.featuresnove;
 
-public class Estagiario {
+public class Estagiario implements Pessoa{
+
+    private String nome;
+    private int idade;
+    private boolean trabalhando = true;
+
+    public Estagiario(String nome, int idade, boolean trabalhando){
+        this.nome = nome;
+        this.idade = idade;
+        this.trabalhando = trabalhando;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+
+    public void expediente(){
+
+        if(!trabalhando){
+            System.out.println(nome + " está no trabalho.");
+        }else System.out.println(nome + " voltou pra casa.");
+    }
+
+    @Override
+    public String dizer() {
+        return null;
+    }
+
+    @Override
+    public void dizerOi() {
+
+    }
+
+    @Override
+    public String dizerTchau() {
+        return null;
+    }
 }
