345b11d91eb3856217cb497f7fe18f0c505f1482
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-28 09:20:43 -0300

    Classe para processar threads de Carro

--- Codigos/src/com/playground/exercicios3/CorridaCarro.java
+++ Codigos/src/com/playground/exercicios3/CorridaCarro.java
@@ -1,4 +1,13 @@
 package com.playground.exercicios3;
 
 public class CorridaCarro {
+    public static void main(String[] args) {
+
+        System.out.println("====Começo da F1 2020====\n");
+
+        Carro carro1 = new Carro("Ferrari", 1000);
+        Carro carro2 = new Carro("Lamborghini", 800);
+        Carro carro3 = new Carro("Porsch", 600);
+
+    }
 }
