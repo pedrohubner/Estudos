458a24bf2cb4f65d7d0f8884b569b852f233cd9d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-28 13:55:00 -0300

    Instanciando objetos da classe Estagiario, da Interface Pessoa e implementando seus métodos

--- Codigos/src/com/semanatres/featuresnove/Beta2.java
+++ Codigos/src/com/semanatres/featuresnove/Beta2.java
@@ -2,6 +2,26 @@ package com.semanatres.featuresnove;
 
 public class Beta2 {
     public static void main(String[] args) {
-        //TODO escrever código das features do java 9
+
+        Estagiario e1 = new Estagiario("Pedro", 18, false);
+
+        Pessoa p1 = new Pessoa() {
+            @Override
+            public String dizer() {
+                return null;
+            }
+        };
+
+        e1.expediente();
+
+        //Implementando métodos privados dentro de métodos default
+        p1.dizerOi();
+
+        e1.setTrabalhando(true);
+
+        //Implementando métodos privados dentro de métodos default
+        System.out.println(p1.dizerTchau());
+
+        e1.expediente();
     }
 }
