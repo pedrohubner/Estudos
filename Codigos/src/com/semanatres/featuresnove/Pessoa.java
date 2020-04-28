4aa998178e4b501593488df161f73e293a3cab38
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-28 13:52:14 -0300

    Criando interface Pessoa com método privado sendo inserido no método default dizerOi e dizerTchau

--- Codigos/src/com/semanatres/featuresnove/Pessoa.java
+++ Codigos/src/com/semanatres/featuresnove/Pessoa.java
@@ -1,4 +1,28 @@
 package com.semanatres.featuresnove;
 
-public class Pessoa {
+
+public interface Pessoa {
+
+    default String dizer() {
+        saudar();
+        return dizer();
+    }
+
+    default void dizerOi() {
+        saudar();
+    }
+
+    default String dizerTchau() {
+        return darTchau();
+    }
+
+    private void saudar() {
+        System.out.println("Oi, tudo bem?");
+    }
+
+    //Método privado sendo implementado nos métodos default acima
+    private String darTchau() {
+        return "Até mais, se cuida!";
+
+    }
 }
