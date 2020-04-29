17a28c70ab41938f376c3a3eab172344713686ad
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-29 14:44:11 -0300

    Criando objeto revista4 para clonar revista3

--- Codigos/src/com/semanadois/copy/Copias.java
+++ Codigos/src/com/semanadois/copy/Copias.java
@@ -1,15 +1,33 @@
-package com.semanadois;
+package com.semanadois.copy;
 
 public class Copias {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException{
+
+        Revista revista1 = new Revista("Amazing Fantasy ", "#15", 21, 150.9, true);
+        Revista revista2 = new Revista("Detective Comics ", "#27", 23, 498.9, true);
+        Revista revista3 = new Revista("Nightwing ", "#152", 32, 10.9, false);
+
+        Revista revista4 = (Revista) revista2.clone();
+
+        System.out.println(revista4);
+
+        revista4.setName("Action Comics ");
+        revista4.setEdicao("#12");
+        revista4.setQntdPags(36);
+        revista4.setPreco(190.6);
+
+        System.out.println(revista4);
 
         /**
          * SHALLOW COPY
          */
 
 
+
         /**
          * DEEP COPY
          */
+
+
     }
 }
