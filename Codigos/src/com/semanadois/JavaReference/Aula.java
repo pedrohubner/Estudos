63405d86882c1baaf7f8daa8cd105deef941e0d4
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-29 12:17:12 -0300

    Add método estático alterarEmail com acesso por referência do a3, e add método exbirCarteiraEstudante com acesso a referência do a1

--- Codigos/src/com/semanadois/JavaReference/Aula.java
+++ Codigos/src/com/semanadois/JavaReference/Aula.java
@@ -1,4 +1,31 @@
 package com.semanadois.JavaReference;
 
-public class Mecanico {
+public class Aula {
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Luana", "lusilva@yahoo.com.br", 21, 192786);
+        Aluno a2 = new Aluno("Rodrigo", "rodri@outlook.com", 19, 623487);
+        Aluno a3 = new Aluno("Mathias", "mat@gmail.com", 20, 678524);
+
+        //Acessando valor
+        alterarEmail(a3);
+
+        System.out.println("\n" + a3.getNome() + " alterou seu email para " + a3.getEmail());
+
+        //Acessando referência
+        exibirCarteiraEstudante(a1);
+
+        System.out.println("Documento de " + a1.getNome() + ": " + a1.getId());
+
+    }
+
+    //Passando parâmetro de referência email
+    public static void alterarEmail(Aluno a3){
+        a3.setEmail("mathiaspereira@gmail.com");
+    }
+
+    //Passando parâmetro de referência id
+    public static void exibirCarteiraEstudante(Aluno a1){
+        a1.getId();
+    }
 }
