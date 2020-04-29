b4a7beabc29962f6b277bad9c820c3c2cfcd875a
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-04-29 12:15:22 -0300

    Add método fazerAniversario com exemplo de passagem de parâmetros de valores e referências

--- Codigos/src/com/playground/exercicios3/TesteReferencia.java
+++ Codigos/src/com/playground/exercicios3/TesteReferencia.java
@@ -1,4 +1,23 @@
 package com.playground.exercicios3;
 
 public class TesteReferencia {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa("Pedro", 78723);
+
+        int idade = 18;
+
+        fazerAniversario(idade, p1);
+
+        System.out.println(p1);
+        System.out.println(idade);
+
+    }
+
+    public static void fazerAniversario(int idade, Pessoa p1){
+        int novaIdade = idade + 1;
+        idade = novaIdade;
+
+        p1.setNome("Pedro Naã tem " + novaIdade);
+    }
 }
