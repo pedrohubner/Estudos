14f90779dd200119f0e864e731df8aea7ec53e84
Pedro Hübner <pedronhubner@gmail.com>
2020-05-02 21:50:49 -0300

    Excluindo try/catch para corredor dois e add finally com release para um novo corredor

--- Codigos/src/com/semanadois/threads/semaforo/Corredor.java
+++ Codigos/src/com/semanadois/threads/semaforo/Corredor.java
@@ -50,33 +50,10 @@ public class Corredor implements Runnable{
 
         }catch (InterruptedException e){
             e.printStackTrace();
+        } finally {
+            System.out.println("Pátio vazio");
+            sem.release();
         }
-        System.out.println("Pátio vazio");
-        sem.release();
-
-//        //Corredor 2
-//        System.out.println("\n" + nome + " entra no estádio");
-//
-//        try {
-//            System.out.println(nome + " está esperando o início da corrida");
-//
-//            sem.acquire();
-//
-//            System.out.println(nome + " começa a correr\n");
-//
-//            for (int i = 0; i < 10; i++){
-//                Arbitro.contagem++;
-//                System.out.println(nome + ":" + Arbitro.contagem + "km");
-//
-//                Thread.sleep(100);
-//            }
-//            System.out.println("\n" + nome + " conclui corrida");
-//
-//        }catch (InterruptedException e){
-//            e.printStackTrace();
-//        }
-//        System.out.println("Pátio vazio");
-//        sem.release();
 
     }
 }
