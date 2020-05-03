85166bf3007058204fa06ef471edd22d4085dffb
Pedro Hübner <pedronhubner@gmail.com>
2020-05-02 21:47:51 -0300

    Instanciando objetos corredor1 e corredor2 da classe Corredor e imprimindo método run

--- Codigos/src/com/semanadois/threads/semaforo/Corrida.java
+++ Codigos/src/com/semanadois/threads/semaforo/Corrida.java
@@ -1,4 +1,16 @@
 package com.semanadois.threads.semaforo;
 
+import java.util.concurrent.Semaphore;
+
 public class Corrida {
+    public static void main(String[] args) {
+
+        Semaphore sem = new Semaphore(1);
+
+        Corredor corredor1 = new Corredor(sem, "Pedro");
+        Corredor corredor2 = new Corredor(sem, "Luana");
+
+        corredor1.run();
+        corredor2.run();
+    }
 }
