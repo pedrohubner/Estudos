7117f985770f9f42da182d33101cbf2079128061
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-04 15:03:34 -0300

    Add ArrayList para criar lista de produtos e imprimindo método comprarProduto com method references

--- Codigos/src/com/playground/miniprojeto/Cliente.java
+++ Codigos/src/com/playground/miniprojeto/Cliente.java
@@ -1,4 +1,73 @@
 package com.playground.miniprojeto;
 
 public class Cliente {
+
+    private String nome;
+    private String endereco;
+    private String email;
+    private long telefone;
+    private String cpf;
+    private double qntdDinheiro;
+
+    public Cliente(String nome, String endereco, String email, long telefone, String cpf, double qntdDinheiro){
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.qntdDinheiro = qntdDinheiro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getQntdDinheiro() {
+        return qntdDinheiro;
+    }
+
+    public void setQntdDinheiro(double qntdDinheiro) {
+        this.qntdDinheiro = qntdDinheiro;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nome + "\nEndereço: " + endereco + "\nTelefone: " + telefone;
+    }
 }
--- Codigos/src/com/playground/miniprojeto/Compras.java
+++ Codigos/src/com/playground/miniprojeto/Compras.java
@@ -1,4 +1,37 @@
 package com.playground.miniprojeto;
 
+import java.util.ArrayList;
+
 public class Compras {
+    public static void main(String[] args) {
+
+        ArrayList <Produtos> listaProdutos = new ArrayList<>();
+        ArrayList <Produtos> listaProdutos2 = new ArrayList<>();
+
+        Loja loja1 = new Loja("Panvel", "Zeca Neto, 48", 993309278);
+        Loja loja2 = new Loja("Droga Raia", "Dorival Candido 55", 991324971);
+
+        Produtos produto1 = new Produtos("Shampoo Axe", 8.5, 20, loja1);
+        Produtos produto2 = new Produtos("Shampoo Rexona", 5.3, 17, loja1);
+        Produtos produto3 = new Produtos("Shampoo Clear", 7.6, 12, loja1);
+
+        Produtos produto4 = new Produtos("Shampoo Axe", 8.5, 20, loja2);
+        Produtos produto5 = new Produtos("Shampoo Rexona", 5.3, 17, loja2);
+        Produtos produto6 = new Produtos("Shampoo Clear", 7.6, 12, loja2);
+
+        //Produtos loja 1
+        listaProdutos.add(produto1);
+        listaProdutos.add(produto2);
+        listaProdutos.add(produto3);
+
+        //Produtos loja 2
+        listaProdutos2.add(produto4);
+        listaProdutos2.add(produto5);
+        listaProdutos2.add(produto6);
+
+        listaProdutos.forEach(Produtos::comprarProduto);
+
+
+
+    }
 }
