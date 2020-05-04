8238917ec36ed114149645119f678a301693178d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-04 08:47:04 -0300

    Add dois métodos abstratos e deletando método default

--- Codigos/src/com/semanatres/featuresoito/OpBasicas.java
+++ Codigos/src/com/semanatres/featuresoito/OpBasicas.java
@@ -2,18 +2,11 @@ package com.semanatres.featuresoito;
 
 public interface OpBasicas {
 
-    //Método default
-    default public int soma(){
-        int a = 87;
-        int b = 55;
+    public void mediaSem1();
 
-        int soma = a + b;
-
-        System.out.println("\nSoma de 87 + 55 = " + soma);
-        return soma;
-    }
+    public void mediaSem2();
 
     //único método abstrato dessa interface funcional
-    public void subtrai();
+    public void passouDeAno();
 
 }
