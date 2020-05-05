157ca77134df55b79f8af3171e286248b5780984
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-05 14:15:00 -0300

    Instanciando objeto g1 de GerenciadorArquivos e implementando seus métodos

--- Codigos/src/com/semanatres/featuresnove/gaveta/ArquivoUm.java
+++ Codigos/src/com/semanatres/featuresnove/gaveta/ArquivoUm.java
@@ -1,4 +1,14 @@
 package com.semanatres.featuresnove.gaveta;
 
 public class ArquivoUm {
+    public static void main(String[] args) {
+
+        GerenciadorDeArquivos g1 = new GerenciadorDeArquivos();
+
+        g1.criarArquivo();
+
+        g1.EscreverArquivo();
+
+        g1.LerArquivo();
+    }
 }
