02c864af9fd9638a7bd63e2c6f6e0ee6d15ea9f5
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-05 14:14:25 -0300

    Criando uma nova classe e utilizando Scanner para digitar nome do arquivo junto com seu conteúdo

---
+++ Codigos/src/com/semanatres/featuresnove/gaveta/ArquivoUm.java
@@ -0,0 +1,4 @@
+package com.semanatres.featuresnove.gaveta;
+
+public class ArquivoUm {
+}
---
+++ Codigos/src/com/semanatres/featuresnove/gaveta/GerenciadorDeArquivos.java
@@ -0,0 +1,60 @@
+package com.semanatres.featuresnove.gaveta;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class GerenciadorDeArquivos {
+    private Scanner nome = new Scanner(System.in);
+    private Scanner conteudo = new Scanner(System.in);
+
+    public Scanner getNome() {
+        return nome;
+    }
+
+//    public void criarArquivo(){
+//
+//        System.out.println("Digite o nome do arquivo: ");
+//        File file = new File(nome.next());
+//
+//            try {
+//                boolean value = file.createNewFile();
+//                if (value){
+//                    System.out.println("Arquivo criado");
+//                }else System.out.println("Arquivo já existe.");
+//            }catch (IOException e){
+//                e.printStackTrace();
+//            }
+//    }
+
+    public void EscreverArquivo(){
+
+        System.out.println("Digite o conteúdo do arquivo: ");
+        String data = conteudo.next();
+
+            try {
+                System.out.println("Digite o nome do arquivo: ");
+                FileWriter output = new FileWriter(nome.next());
+
+                output.write(data);
+
+                System.out.println("Escreveram no arquivo");
+
+                output.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+    }
+
+    public void LerArquivo(){
+
+        try (BufferedReader br = new BufferedReader(new FileReader("DOcumentos"))){
+            String arquivo =  br.readLine();
+            while (arquivo != null){
+                System.out.println(arquivo);
+                arquivo = br.readLine();
+            }
+        }catch (IOException e){
+            e.getStackTrace();
+        }
+    }
+}
