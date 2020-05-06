ec2553123876093c4500a27b535837d2232f6a2c
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-06 15:00:48 -0300

    Comentando ArrayList e sua implementação e implementando método reporEstoque através do objeto produto3

--- Codigos/src/com/playground/miniprojeto/Compras.java
+++ Codigos/src/com/playground/miniprojeto/Compras.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Compras {
     public static void main(String[] args) {
 
-        ArrayList <Produtos> listaProdutos = new ArrayList<>();
-        ArrayList <Produtos> listaProdutos2 = new ArrayList<>();
+//        ArrayList <Produtos> listaProdutos = new ArrayList<>();
+//        ArrayList <Produtos> listaProdutos2 = new ArrayList<>();
 
         Loja loja1 = new Loja("Panvel", "Zeca Neto, 48", 993309278);
         Loja loja2 = new Loja("Droga Raia", "Dorival Candido 55", 991324971);
@@ -15,21 +15,24 @@ public class Compras {
         Produtos produto2 = new Produtos("Shampoo Rexona", 5.3, 17, loja1);
         Produtos produto3 = new Produtos("Shampoo Clear", 7.6, 12, loja1);
 
-        Produtos produto4 = new Produtos("Shampoo Dove", 8.5, 0, loja2);
+        Produtos produto4 = new Produtos("Shampoo Dove", 8.5, 20, loja2);
         Produtos produto5 = new Produtos("Shampoo Pantene", 5.3, 17, loja2);
         Produtos produto6 = new Produtos("Shampoo Head & Sholder's", 7.6, 12, loja2);
 
-        //Produtos loja 1
-        listaProdutos.add(produto1);
-        listaProdutos.add(produto2);
-        listaProdutos.add(produto3);
+//        //Produtos loja 1
+//        listaProdutos.add(produto1);
+//        listaProdutos.add(produto2);
+//
+//        listaProdutos.add(produto3);
+//
+//        //Produtos loja 2
+//        listaProdutos2.add(produto4);
+//        listaProdutos2.add(produto5);
+//        listaProdutos2.add(produto6);
+//
+//        System.out.println(listaProdutos);
 
-        //Produtos loja 2
-        listaProdutos2.add(produto4);
-        listaProdutos2.add(produto5);
-        listaProdutos2.add(produto6);
-
-        System.out.println(produto4.comprarProduto());
+        System.out.println(produto3.reporEstoque());
 
     }
 }
