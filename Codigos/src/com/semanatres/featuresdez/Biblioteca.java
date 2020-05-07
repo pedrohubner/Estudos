60ccb942e27c2346cd983724be2e1497fa9e0eda
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-07 10:08:44 -0300

    Criando var setNomes que contém um Set de nomes completos

--- Codigos/src/com/semanatres/featuresdez/Biblioteca.java
+++ Codigos/src/com/semanatres/featuresdez/Biblioteca.java
@@ -1,19 +1,24 @@
 package com.semanatres.featuresdez;
 
 import java.util.List;
+import java.util.Set;
 
 public class Biblioteca {
     public static void main(String[] args) {
 
-        //Usando inferências da variáveis locais
-        var livro1 = "Altered Carbon";
-        var livro2 = "Duna";
-        var livro3 =  "Fundação";
-        var listaDeLivros = List.of("Fundação", "Altered Carbon", "Duna");
+        System.out.println("\nlista de livros:");
 
-        System.out.println("\nLivros que encontrei na biblioteca ");
-        for (var livro:listaDeLivros) {
-            System.out.println(livro);
-        }
+        var livro1 = "- Altered Carbon";
+        var livro2 = "- Duna";
+        var livro3 =  "- Fundação";
+        var listaDeLivros = List.of(livro1, livro2, livro3);
+
+        listaDeLivros.forEach(System.out::println);
+
+        System.out.println("\nLista de nomes:");
+
+        var setNomes = Set.of("- Samuel Meister Hübner", "- Davi Arthur Hübner");
+        setNomes.forEach(System.out::println);
+
     }
 }
