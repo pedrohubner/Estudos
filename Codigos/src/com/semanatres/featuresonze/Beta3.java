1c79c530208946f933c1edc53f79f2ad0b3579e5
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-07 10:10:14 -0300

    Criando exemplo com String nomes.isBlank() e fazendo stream com nomes.line()

--- Codigos/src/com/semanatres/featuresonze/Beta3.java
+++ Codigos/src/com/semanatres/featuresonze/Beta3.java
@@ -1,7 +1,24 @@
 package com.semanatres.featuresonze;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 public class Beta3 {
     public static void main(String[] args) {
-        //TODO escrever código das features do java 11
+
+        String nomes = "Pedro\nNaã\nHübner";
+        System.out.println(nomes);
+
+        //Verificando se a String tá vazia
+        System.out.println(nomes.isBlank());
+
+        //Transformando string em uma coleção(uma lista)
+        nomes.lines()
+                .map(n -> "/" + nomes)
+                .forEach(System.out::println);
+
     }
 }
