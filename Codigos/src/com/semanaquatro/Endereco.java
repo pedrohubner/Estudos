a12ca19603662351360a29993c315d607f887ad1
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-08 12:15:58 -0300

    Implementando toString

--- Codigos/src/com/semanaquatro/Endereco.java
+++ Codigos/src/com/semanaquatro/Endereco.java
@@ -1,4 +1,46 @@
 package com.semanaquatro;
 
 public class Endereco {
+
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "rua " + rua + ", número " + numero + ", bairro " + bairro;
+    }
 }
