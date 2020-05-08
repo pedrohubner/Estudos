636c8264f6c30b2de63a5492a5bf6ddf9c20986d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-08 14:51:06 -0300

    Criando atributos Pessoa pessoa e Endereco endereco, criando construtor e método estático pessoa

--- Codigos/src/com/semanaquatro/TesteBuilder.java
+++ Codigos/src/com/semanaquatro/TesteBuilder.java
@@ -1,4 +1,16 @@
 package com.semanaquatro;
 
 public class TesteBuilder {
+
+    private Pessoa pessoa;
+    private Endereco endereco;
+
+    public TesteBuilder(){
+        this.pessoa = new Pessoa();
+        this.endereco = new Endereco();
+    }
+
+    public static TesteBuilder pessoa(){
+        return new TesteBuilder();
+    }
 }
