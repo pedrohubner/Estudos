a3b7df6a778ca6d35d2829846e4ebe5ee3643bf2
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-08 12:16:40 -0300

    Criando classe para exemplo de objeto builder

--- Codigos/src/com/semanaquatro/Beta4.java
+++ Codigos/src/com/semanaquatro/Beta4.java
@@ -1,4 +1,7 @@
 package com.semanaquatro;
 
 public class Beta4 {
+    public static void main(String[] args) {
+        System.out.println("Classe para exemplos dos conteúdos da semana quatro");
+    }
 }
