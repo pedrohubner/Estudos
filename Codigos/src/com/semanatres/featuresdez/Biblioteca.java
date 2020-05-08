ee08905609a74aca61c7a6c9a546288d4fd8d6ae
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-08 11:32:25 -0300

    Criando cópia de List estante com Set.copyOf e implementando método exibirLivros e checarEstoque

--- Codigos/src/com/semanatres/featuresdez/Biblioteca.java
+++ Codigos/src/com/semanatres/featuresdez/Biblioteca.java
@@ -1,26 +1,46 @@
 package com.semanatres.featuresdez;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Biblioteca {
     public static void main(String[] args) {
 
-        System.out.println("\nlista de livros:");
+        var num = 20;
 
-        var livro1 = "- Altered Carbon";
-        var livro2 = "- Duna";
-        var livro3 =  "- Fundação";
-        var listaDeLivros = List.of(livro1, livro2, livro3);
+        System.out.println("\nSérie A:");
 
-        listaDeLivros.forEach(System.out::println);
+        var time1 = "- Internacional";
+        var time2 = "- São Paulo";
+        var time3 = "- Chapecoense";
+        var classificacao = List.of(time1, time2, time3);
 
-        System.out.println("\nLista de nomes:");
+        classificacao.forEach(System.out::println);
 
-        var setNomes = Set.of("- Samuel Meister Hübner", "- Davi Arthur Hübner");
-        setNomes.forEach(System.out::println);
+        Livros livro = new Livros("\"Blade Runner\"", "Aleph", 252, 59.9,
+                5);
+        Livros livro2 = new Livros("\"O Homem No Castelo Alto\"", "Aleph", 376, 42.5,
+                2);
+        Livros livro3 = new Livros("\"Eu, Robô\"", "Aleph", 460, 39.9,
+                6);
 
-        //TODO Adicionar exemplo garbage collector e versionamento com data
+        List<Livros> estante = new ArrayList<>();
+
+        estante.add(livro);
+        estante.add(livro2);
+        estante.add(livro3);
+
+        Set<Livros> novaEstante = Set.copyOf(estante);
+
+        System.out.println("\nLista de livros disponíveis na loja:");
+
+        novaEstante.forEach(Livros::exibirLivros);
+
+        System.out.println("\n" + livro2.checarEstoque(2));
+
+//        Map<Integer, Livros> outraNovaEstante = Map.copyOf(num,estante);
 
     }
 }
