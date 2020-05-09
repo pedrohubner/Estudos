f49342c3294faf40c71c96231c43508357f7e48a
Pedro Hübner <pedronhubner@gmail.com>
2020-05-09 20:37:50 -0300

    Criando List listaAluno e implementando stream exibindo nome e matrícula, implementando forEach para exibir verificaAprovacao com expressão lambda e outro forEach para imprimir exibeDados com referência de método

--- Codigos/src/com/semanatres/featuresoito/Aula.java
+++ Codigos/src/com/semanatres/featuresoito/Aula.java
@@ -1,46 +1,33 @@
 package com.semanatres.featuresoito;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Aula {
     public static void main(String[] args) throws Exception{
 
-        /**
-         * EXEMPLOS DE LAMBDA EXPRESSIONS E METHOD REFERENCES
-         */
-        ArrayList <Aluno> boletim = new ArrayList<>();
+        Aluno a1 = new Aluno("João", 63185249L, 218, 8);
+        Aluno a2 = new Aluno("Luciane", 3854752L, 202, 7);
+        Aluno a3 = new Aluno("Luana", 14578238L, 101, 7);
+        Aluno a4 = new Aluno("Carl", 2447523L, 301, 6);
 
-        Aluno a1 = new Aluno("João", 63185249, 218, 80, 60);
-        Aluno a2 = new Aluno("Luciane", 3854752, 202, 60, 89);
-        Aluno a3 = new Aluno("Luana", 14578238, 101, 70, 70);
-        Aluno a4 = new Aluno("Carl", 2447523, 301, 90, 90);
+        List<Aluno> listaAluno = new ArrayList<>();
 
-        //Criando threads para cada aluno e imprimindo usando lambda expression
-        Thread t1 = new Thread(() -> System.out.println(a1));
-        Thread t2 = new Thread(() -> System.out.println(a2));
-        Thread t3 = new Thread(() -> System.out.println(a3));
-        Thread t4 = new Thread(() -> System.out.println(a4));
+        listaAluno.add(a1);
+        listaAluno.add(a2);
+        listaAluno.add(a3);
+        listaAluno.add(a4);
 
-        boletim.add(a1);
-        boletim.add(a2);
-        boletim.add(a3);
-        boletim.add(a4);
-
-//        t1.start();
-//        t2.start();
-//        t3.start();
-//        t4.start();
-
-        boletim.stream()
-                .filter(a -> a.getNome().startsWith("C"))
-                .map(a -> a.getNotaSemestre2())
+        System.out.println("\nExibindo matrícula do aluno e seu nome:");
+        listaAluno.stream()
+                .map(a -> a.getMatricula() + " - " + a.getNome())
                 .forEach(System.out::println);
 
-        a1.passouDeAno();
+        System.out.println("\nVerificando dados dos alunos: ");
+        listaAluno.forEach(a -> System.out.println(a.verificarAprovacao()));
 
-//        System.out.println("====Exibindo dados dos alunos====");
-//        //método da instância de um objeto particular
-//        boletim.forEach(Aluno::exibeDados);
+        System.out.println("\nVerificando alunos aprovados:");
+        listaAluno.forEach(Aluno::exibeDados);
 
     }
 }
