d7a34929703b2a216dabfb2c02e4e04878ecd805
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-11 14:19:51 -0300

    Instanciando objeto pessoaBuilder de PessoaBuilder, implementando seus atributos e copiando eles para objeto pessoa de Pessoa através do método get

--- Codigos/src/com/semanaquatro/Beta4.java
+++ Codigos/src/com/semanaquatro/Beta4.java
@@ -3,5 +3,18 @@ package com.semanaquatro;
 public class Beta4 {
     public static void main(String[] args) {
         System.out.println("Classe para exemplos dos conteúdos da semana quatro");
+
+        PessoaBuilder pessoaBuilder = new PessoaBuilder()
+                .addNome("Pedro", "Hübner")
+                .addIdade(18)
+                .addDataNascimento(20, 11, 2001)
+                .addRua("Forest Hill")
+                .addNumero(616)
+                .addBairro("Queens")
+                .addCidade("New York");
+
+        Pessoa pessoa = pessoaBuilder.get();
+
+        System.out.println("\n" + pessoa);
     }
 }
