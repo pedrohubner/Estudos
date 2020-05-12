cec1c890043423a55350bad676d4c45a4b9ce09e
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-12 12:48:07 -0300

    Criando método verificarAprovacaoBimestre

---
+++ Codigos/src/com/playground/miniprojeto/Estudante.java
@@ -0,0 +1,63 @@
+package com.playground.miniprojeto;
+
+public class Estudante {
+
+    private String nome;
+    private String email;
+    private Integer matricula;
+    private Integer nota1;
+    private Integer nota2;
+
+    public Estudante(String nome, String email, Integer matricula, Integer nota1, Integer nota2){
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(Integer nota1) {
+        this.nota1 = nota1;
+    }
+
+    public Integer getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Integer nota2) {
+        this.nota2 = nota2;
+    }
+
+    public Integer verificarAprovacaoBimestre(){
+        int somaNotas = (nota1 + nota2) / 2;
+        return somaNotas;
+    }
+}
