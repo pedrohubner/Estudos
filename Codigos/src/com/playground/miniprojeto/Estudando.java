021786a30be1ad444c24e35039a853f668d2c7bf
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-12 12:48:31 -0300

    Criando laço for para a implementação do método verificarAprovacaoBimestre

--- Codigos/src/com/playground/miniprojeto/Estudando.java
+++ Codigos/src/com/playground/miniprojeto/Estudando.java
@@ -1,4 +1,16 @@
 package com.playground.miniprojeto;
 
 public class Estudando {
+    public static void main(String[] args) {
+
+        Estudante e1 = new Estudante("Julia", "j@gmail.com", 642581, 7, 6);
+
+        for (int i = 0; i < 5; i++){
+            if (e1.verificarAprovacaoBimestre() >= 6)
+                System.out.println(e1.getNome().concat(" passou no bimestre. Nota final: ")  +
+                        e1.verificarAprovacaoBimestre());
+            else System.out.println(e1.getNome() + " reprovou no bimestre. Nota final: " +
+                    e1.verificarAprovacaoBimestre());
+        }
+    }
 }
