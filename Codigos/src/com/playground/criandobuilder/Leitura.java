d62799059c2d72a842aed5f6c546e77fcb33888d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-13 14:02:32 -0300

    Instanciando builders de Blog e Revista

--- Codigos/src/com/playground/criandobuilder/Leitura.java
+++ Codigos/src/com/playground/criandobuilder/Leitura.java
@@ -1,4 +1,25 @@
 package com.playground.criandobuilder;
 
 public class Leitura {
+    public static void main(String[] args) {
+
+        Blog blog = new BlogBuilder()
+                .addTitulo("Testando Builder")
+                .addPublicacao( "Pensamentos de estagiários", "Estagiário",
+                        "13/05/20", "Puutz")
+                .get();
+
+        //System.out.println(blog);
+
+        Revista revista = new RevistaBuilder()
+                .addTitulo("Época")
+                .addPreco(8.5)
+                .addEditora("Abril")
+                .addQntdPaginas(30)
+                .addPublicacao( "Pensamentos de um Ex-Ministro", "Sérgio Moro",
+                        "12/04/20", "Valeu, rapaziada!")
+                .get();
+
+        System.out.println(revista);
+    }
 }
