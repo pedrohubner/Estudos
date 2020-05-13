1110863f4cde40cbab0a16598e4e68a605b68618
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-13 14:00:25 -0300

    Criando builder para classe revista e adicionando método para criar uma publicação

--- Codigos/src/com/playground/criandobuilder/RevistaBuilder.java
+++ Codigos/src/com/playground/criandobuilder/RevistaBuilder.java
@@ -1,12 +1,42 @@
 package com.playground.criandobuilder;
 
-public class LeituraBuilder {
+public class RevistaBuilder {
 
     private Revista revista;
-    private Blog blog;
 
-    public LeituraBuilder(){
+    public RevistaBuilder(){
         this.revista = new Revista();
-        this.blog = new Blog();
+    }
+
+    public RevistaBuilder addTitulo(String titulo){
+        this.revista.setTitulo(titulo);
+        return this;
+    }
+
+    public RevistaBuilder addPreco(double preco){
+        this.revista.setPreco(preco);
+        return this;
+    }
+
+    public RevistaBuilder addQntdPaginas(int qntdPaginas){
+        this.revista.setQntdPaginas(qntdPaginas);
+        return this;
+    }
+
+    public RevistaBuilder addEditora(String editora){
+        this.revista.setEditora(editora);
+        return this;
+    }
+
+    public RevistaBuilder addPublicacao(String tituloPublicacao, String nomeAutor, String dataPublicacao, String texto){
+        this.revista.tituloPublicacao =tituloPublicacao;
+        this.revista.nomeAutor = nomeAutor;
+        this.revista.dataPublicacao = dataPublicacao;
+        this.revista.texto = texto;
+        return this;
+    }
+
+    public Revista get(){
+        return this.revista;
     }
 }
