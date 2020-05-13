c0da49abfccfe4a0157ecdb6185f9883cfe43b15
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-13 14:02:08 -0300

    Criando builder e add método criar publicação

--- Codigos/src/com/playground/criandobuilder/BlogBuilder.java
+++ Codigos/src/com/playground/criandobuilder/BlogBuilder.java
@@ -1,4 +1,27 @@
 package com.playground.criandobuilder;
 
 public class BlogBuilder {
+
+    private Blog blog;
+
+    public BlogBuilder(){
+        this.blog = new Blog();
+    }
+
+    public BlogBuilder addTitulo(String titulo){
+        this.blog.setTitulo(titulo);
+        return this;
+    }
+
+    public BlogBuilder addPublicacao(String tituloPublicacao, String nomeAutor, String dataPublicacao, String texto){
+        this.blog.tituloPublicacao = tituloPublicacao;
+        this.blog.nomeAutor = nomeAutor;
+        this.blog.dataPublicacao = dataPublicacao;
+        this.blog.texto = texto;
+        return this;
+    }
+
+    public Blog get(){
+        return this.blog;
+    }
 }
