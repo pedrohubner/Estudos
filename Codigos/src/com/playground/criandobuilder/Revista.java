aaef1c39423318f07249c9f5ec4fb1400e9b4a88
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-13 13:59:33 -0300

    Add getters e setters aos atributos e também método toString que chama atributos de Publicacao

--- Codigos/src/com/playground/criandobuilder/Revista.java
+++ Codigos/src/com/playground/criandobuilder/Revista.java
@@ -1,4 +1,47 @@
 package com.playground.criandobuilder;
 
-public class Revista {
+public class Revista extends Publicacao{
+
+    private String titulo;
+    private double preco;
+    private int qntdPaginas;
+    private String editora;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQntdPaginas() {
+        return qntdPaginas;
+    }
+
+    public void setQntdPaginas(int qntdPaginas) {
+        this.qntdPaginas = qntdPaginas;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTrecho da revista " + titulo + "\n=======================\n" + tituloPublicacao + ":\n\"" + texto +
+                "\"\n" + nomeAutor + " - " + dataPublicacao;
+    }
 }
