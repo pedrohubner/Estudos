972053c16bc8f27d155678eda12f03fcb8ce589d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-13 14:01:16 -0300

    Add getters e setter aos atributos e método toString chamando atributos de publicação

--- Codigos/src/com/playground/criandobuilder/Blog.java
+++ Codigos/src/com/playground/criandobuilder/Blog.java
@@ -1,4 +1,21 @@
 package com.playground.criandobuilder;
 
-public class Blog {
+public class Blog extends Publicacao {
+
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + tituloPublicacao + "\"\n" + this.nomeAutor + " - " + this.dataPublicacao + "\n==========\n"
+                + this.texto;
+    }
 }
+
