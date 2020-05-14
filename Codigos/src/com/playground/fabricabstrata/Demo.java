e677f57afbf7d31b0f30af8f2521e17872868ceb
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-14 12:18:35 -0300

    Criando método estático pegandoControle do tipo ApertarBotao, que cria um atributo ab do tipo ApertarBotao e um prototipo do tipo FabricaControle, criando atributo tipoBotao do tipo String que usa um getProperty

--- Codigos/src/com/playground/fabricabstrata/Demo.java
+++ Codigos/src/com/playground/fabricabstrata/Demo.java
@@ -1,4 +1,33 @@
 package com.playground.fabricabstrata;
 
+import com.playground.fabricabstrata.cliente.ApertarBotao;
+import com.playground.fabricabstrata.fabrica.FabricaControle;
+import com.playground.fabricabstrata.fabrica.FabricaControlePortao;
+import com.playground.fabricabstrata.fabrica.FabricaControleTV;
+import com.playground.fabricabstrata.fabrica.FabricaControleVideoGame;
+
 public class Demo {
+
+    public static ApertarBotao pegandoControle(){
+        ApertarBotao ab;
+        FabricaControle prototipo;
+
+        String tipoBotao = System.getProperty("key", "videogame");
+        if (tipoBotao.contains("videogame")) {
+            prototipo = new FabricaControleVideoGame();
+            ab = new ApertarBotao(prototipo);
+        } else if (tipoBotao.contains("tv")){
+            prototipo = new FabricaControleTV();
+            ab = new ApertarBotao(prototipo);
+        } else {
+            prototipo = new FabricaControlePortao();
+            ab = new ApertarBotao(prototipo);
+        }
+        return ab;
+    }
+
+    public static void main(String[] args) {
+        ApertarBotao ab = pegandoControle();
+        ab.botao();
+    }
 }
