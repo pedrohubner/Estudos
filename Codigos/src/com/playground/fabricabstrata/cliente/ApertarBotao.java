b4c7e7f4bba172359c908bf46d02cc600ebcb3a5
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-14 12:16:30 -0300

    Criando atributo do tipo Controle controle, add construtor com parâmetro fabrica de Fabrica controle, que retorna o método criarBotao ao atributo controle, e criando método botao, que exibe o método botao() da interface Controle

--- Codigos/src/com/playground/fabricabstrata/cliente/ApertarBotao.java
+++ Codigos/src/com/playground/fabricabstrata/cliente/ApertarBotao.java
@@ -1,9 +1,18 @@
 package com.playground.fabricabstrata.cliente;
 
+import com.playground.fabricabstrata.fabrica.FabricaControle;
 import com.playground.fabricabstrata.projeto.Controle;
 
-public class Apertar {
+public class ApertarBotao {
 
     private Controle controle;
+
+    public ApertarBotao(FabricaControle fabrica) {
+        controle = fabrica.criarBotao();
+    }
+
+    public void botao(){
+        controle.botao();
+    }
 
 }
