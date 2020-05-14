33c89c52ddcd0c972e264e05d7b69495d77f4360
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-14 12:09:53 -0300

    Implementando interfae Controle e seu método abstrato botao

--- Codigos/src/com/playground/fabricabstrata/projeto/ControleTV.java
+++ Codigos/src/com/playground/fabricabstrata/projeto/ControleTV.java
@@ -1,11 +1,9 @@
 package com.playground.fabricabstrata.projeto;
 
-import com.playground.fabricabstrata.projeto.Controle;
-
 public class ControleTV implements Controle {
 
     @Override
     public void botao(){
-        System.out.println("Aperte o botão para ligar a TV.");
+        System.out.println("Aperte o botão do controle para ligar a TV.");
     }
 }
