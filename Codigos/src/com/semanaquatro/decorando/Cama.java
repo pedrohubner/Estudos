fa78e625a78be60a66b8312b8b8a9d51da86c2ff
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-15 10:14:08 -0300

    Estendendo classe de DecoracaoQuarto e criando construtor que recebe parâmetro Quarto, criando método addCama e implementando método da interface Quarto que usa addCama()

--- Codigos/src/com/semanaquatro/decorando/Cama.java
+++ Codigos/src/com/semanaquatro/decorando/Cama.java
@@ -1,4 +1,24 @@
 package com.semanaquatro.decorando;
 
-public class Cama {
+public class Cama extends DecoracaoQuarto{
+
+
+    public Cama(Quarto quarto){
+        super(quarto);
+    }
+
+    @Override
+    public void decorar() {
+        addCama();
+        quarto.decorar();
+    }
+
+    public void addCama(){
+        System.out.println("Coloquei uma cama no meu quarto.");
+    }
+
+    @Override
+    public String toString() {
+        return "\nO que achou?";
+    }
 }
