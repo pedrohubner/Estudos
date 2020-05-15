5e9bd484ee613ef885311a9e6a699491a61ed905
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-15 10:16:45 -0300

    Estendendo DecoracaoQuarto e criando método addRoupeiro que é usado dentro do método da Interface Quarto decorar()

--- Codigos/src/com/semanaquatro/decorando/Roupeiro.java
+++ Codigos/src/com/semanaquatro/decorando/Roupeiro.java
@@ -1,4 +1,18 @@
 package com.semanaquatro.decorando;
 
-public class Roupeiro {
+public class Roupeiro extends DecoracaoQuarto {
+
+    public Roupeiro(Quarto quarto){
+        super(quarto);
+    }
+
+    @Override
+    public void decorar() {
+        addRoupeiro();
+        quarto.decorar();
+    }
+
+    public void addRoupeiro(){
+        System.out.println("Coloquei um roupeiro no meu quarto");
+    }
 }
