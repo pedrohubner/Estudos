44f35975278b2eba2282136759bbe291583a02ce
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 08:52:49 -0300

    Declarando classe Console e instanciando objetos de Ps5 e Xeries, e implementando o método vender com ambos objetos

--- Codigos/src/com/semanaquatro/bridge/Beta6.java
+++ Codigos/src/com/semanaquatro/bridge/Beta6.java
@@ -1,7 +1,19 @@
 package com.semanaquatro.bridge;
 
+import com.semanaquatro.bridge.comercio.Console;
+import com.semanaquatro.bridge.comercio.Ps5;
+import com.semanaquatro.bridge.comercio.Xseries;
+import com.semanaquatro.bridge.fabricacao.Lancamento;
+import com.semanaquatro.bridge.fabricacao.Producao;
+
 public class Beta6 {
     public static void main(String[] args) {
-        System.out.println("\nClasse para exemplo de padrão de projeto builder");
+
+        Console console1 = new Ps5(new Producao(), new Lancamento());
+        System.out.println(console1.vender());
+
+        Console console2 = new Xseries(new Producao(), new Lancamento());
+        System.out.println(console2.vender());
+
     }
 }
