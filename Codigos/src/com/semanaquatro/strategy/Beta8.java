f485a8134e8353b13ab005fc73ad49776d12cdbb
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 14:00:56 -0300

    Declarando objeto dog1 da classe Cachorro e instanciando da classe PastorAlemao, declarando objeto dog2 da classe Cachorro e instanciando da classe Labrador

--- Codigos/src/com/semanaquatro/strategy/Beta8.java
+++ Codigos/src/com/semanaquatro/strategy/Beta8.java
@@ -1,4 +1,23 @@
 package com.semanaquatro.strategy;
 
+import com.semanaquatro.strategy.racas.Cachorro;
+import com.semanaquatro.strategy.racas.Labrador;
+import com.semanaquatro.strategy.racas.PastorAlemao;
+
 public class Beta8 {
+    public static void main(String[] args) {
+
+        Cachorro dog1 = new PastorAlemao("Astor");
+
+        System.out.println("\n" + dog1.doEat());
+
+        System.out.println(dog1.doBark());
+
+        Cachorro dog2 = new Labrador("Black");
+
+        System.out.println("\n" + dog2.doBark());
+
+        System.out.println(dog2.doEat());
+
+    }
 }
