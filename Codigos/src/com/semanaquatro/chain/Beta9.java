d518084266d207089c56d50699b95b2569c0e050
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 14:07:32 -0300

    Criando classe para exemplo de Design Pattern

--- Codigos/src/com/semanaquatro/chain/Beta9.java
+++ Codigos/src/com/semanaquatro/chain/Beta9.java
@@ -1,4 +1,8 @@
 package com.semanaquatro.chain;
 
 public class Beta9 {
+    public static void main(String[] args) {
+
+        System.out.println("Classe para exemplo do padrão de projetos Chain of Responsibility");
+    }
 }
