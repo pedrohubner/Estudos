647bd6d7cd9b85c5f8b632e623770dd05f101740
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 08:48:44 -0300

    Alterando corpo do método sobrescrito vender()

--- Codigos/src/com/semanaquatro/bridge/comercio/Ps5.java
+++ Codigos/src/com/semanaquatro/bridge/comercio/Ps5.java
@@ -4,13 +4,12 @@ import com.semanaquatro.bridge.fabricacao.Produto;
 
 public class Ps5 extends Console {
 
-
-    Ps5(Produto produto1, Produto produto2){
+    public Ps5(Produto produto1, Produto produto2){
         super(produto1, produto2);
     }
 
     @Override
     public String vender() {
-        return null;
+        return "Playstation 5" + produto1.produzir() + produto2.produzir();
     }
 }
