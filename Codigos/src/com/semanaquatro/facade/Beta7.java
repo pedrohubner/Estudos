1443385e638eac43c8fcebf7e9486f3adb9bd4fb
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 09:56:21 -0300

    Criando classe para exemplo de uso do padrão de projeto facade

--- Codigos/src/com/semanaquatro/facade/Beta7.java
+++ Codigos/src/com/semanaquatro/facade/Beta7.java
@@ -1,4 +1,7 @@
 package com.semanaquatro.facade;
 
 public class Beta7 {
+    public static void main(String[] args) {
+        System.out.println("Classe para exemplo de padrão de projeto facade");
+    }
 }
