228d35e6ef4dd64f3caa01d07f53a7024b534fd6
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 14:07:12 -0300

    Estendendo classe abstrata Cachorro e implementando seus métodos doBark() e doEat()

--- Codigos/src/com/semanaquatro/strategy/racas/Labrador.java
+++ Codigos/src/com/semanaquatro/strategy/racas/Labrador.java
@@ -1,4 +1,30 @@
 package com.semanaquatro.strategy.racas;
 
-public class Labrador {
+import com.semanaquatro.strategy.alimentacao.RacaoNormal;
+import com.semanaquatro.strategy.comunicacao.LatidoNormal;
+
+public class Labrador extends Cachorro {
+
+    private String nome;
+
+    public Labrador(String nome) {
+
+        comida = new RacaoNormal();
+        latido = new LatidoNormal();
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String doBark() {
+        return nome + latido.latir();
+    }
+
+    @Override
+    public String doEat() {
+        return nome + comida.comer();
+    }
 }
