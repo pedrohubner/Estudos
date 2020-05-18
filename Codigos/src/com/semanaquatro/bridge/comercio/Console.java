a3977de21bdd07cc6ca8d08ebb5a3b220965c69e
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-18 08:51:55 -0300

    Alterando modificadores de acesso dos atributos e construtor da classe Console de public para default

--- Codigos/src/com/semanaquatro/bridge/comercio/Console.java
+++ Codigos/src/com/semanaquatro/bridge/comercio/Console.java
@@ -4,10 +4,10 @@ import com.semanaquatro.bridge.fabricacao.Produto;
 
 abstract public class Console {
 
-    public Produto produto1;
-    public Produto produto2;
+    Produto produto1;
+    Produto produto2;
 
-    public Console(Produto produto1, Produto produto2){
+    Console(Produto produto1, Produto produto2){
         this.produto1 = produto1;
         this.produto2 = produto2;
     }
