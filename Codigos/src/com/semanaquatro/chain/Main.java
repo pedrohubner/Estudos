217f916d19731895aa0d595b9806cfb8498525ab
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 11:41:26 -0300

    Instanciando objetos de EmailNotification, SMSNotification, TeamsNotification e Request

---
+++ Codigos/src/com/semanaquatro/chain/Main.java
@@ -0,0 +1,14 @@
+package com.semanaquatro.chain;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EmailNotification emailNotification = new EmailNotification(null);
+        SMSNotification smsNotification = new SMSNotification(emailNotification);
+        TeamsNotification teamsNotification = new TeamsNotification(smsNotification);
+
+        Request exibirMensagem = new Request("Sms");
+
+        System.out.println(teamsNotification.escolher(exibirMensagem));
+    }
+}
