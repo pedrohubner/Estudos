d66bd400e51a7c5743a664a0dcab1e54694efbf9
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 11:46:41 -0300

    Criando atributo protected do tipo HandlerNotificacao novaNotificacao, crando método abstract do tipo String escolher e criando método String proximo

--- Codigos/src/com/semanaquatro/chain/HandlerNotificacao.java
+++ Codigos/src/com/semanaquatro/chain/HandlerNotificacao.java
@@ -1,4 +1,16 @@
 package com.semanaquatro.chain;
 
-public class HandlerNotificacao {
+public abstract class HandlerNotificacao {
+
+    protected HandlerNotificacao novaNotificacao;
+
+    public HandlerNotificacao(HandlerNotificacao novaNotificacao) {
+        this.novaNotificacao = novaNotificacao;
+    }
+
+    protected abstract String escolher(Request pedido);
+
+    public String proximo(Request pedido){
+        return novaNotificacao.escolher(pedido);
+    }
 }
