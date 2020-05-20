9c84183c30a0b280169c20a918f57358426f53de
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 12:04:17 -0300

    Alterando frase de system.out

--- Codigos/src/com/semanaquatro/facade/Beta7.java
+++ Codigos/src/com/semanaquatro/facade/Beta7.java
@@ -2,6 +2,6 @@ package com.semanaquatro.facade;
 
 public class Beta7 {
     public static void main(String[] args) {
-        System.out.println("Classe para exemplo de padrão de projeto facade");
+        System.out.println("Exemplo facade");
     }
 }
