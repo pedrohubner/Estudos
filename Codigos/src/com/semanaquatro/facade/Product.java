e0c55ae5c5fb33b810bc5dd78c086ee0b02465f5
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 14:30:58 -0300

    Criando atributos produto e preco e implementando método listaProdutos

--- Codigos/src/com/semanaquatro/facade/Product.java
+++ Codigos/src/com/semanaquatro/facade/Product.java
@@ -1,4 +1,27 @@
 package com.semanaquatro.facade;
 
-public class Product {
+public class Product extends Menu {
+
+    private String produto;
+    private double preco;
+
+    public Product(String produto, double preco){
+        this.produto = produto;
+        this.preco = preco;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String listaProdutos() {
+        return "===Lista Produtos==="
+                + "\nProduto: " + produto
+                + "\nPreço: " + preco;
+    }
 }
