f560e5d9116aa5f734fcc71524ee148e78b44c29
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 14:32:31 -0300

    Estendendo classe abstrata Menu e implementando seu método perfilCliente

--- Codigos/src/com/semanaquatro/facade/Client.java
+++ Codigos/src/com/semanaquatro/facade/Client.java
@@ -1,4 +1,39 @@
 package com.semanaquatro.facade;
 
-public class Cliente {
+public class Client extends Menu {
+
+    private String nome;
+    private String sobrenome;
+    private String dataNascimento;
+    private long cpf;
+
+    public Client(String nome, String sobrenome, String dataNascimento, long cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String perfilCliente() {
+        return "=====Perfil cliente====="
+        + "\nNome: " + nome + " " + sobrenome
+        + "\nCPF: " + cpf;
+    }
 }
