553f132a40e944b8ec89fbbab32f5a55c7d8e1a1
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 11:43:29 -0300

    Estendendo HandlerNotificacao e implementando seu método escolher

--- Codigos/src/com/semanaquatro/chain/TeamsNotification.java
+++ Codigos/src/com/semanaquatro/chain/TeamsNotification.java
@@ -1,4 +1,16 @@
 package com.semanaquatro.chain;
 
-public class TeamsNotification {
+public class TeamsNotification extends HandlerNotificacao{
+
+    TeamsNotification(HandlerNotificacao msg) {
+        super(msg);
+    }
+
+    @Override
+    protected String escolher(Request pedido) {
+        if (pedido.getTipoMensagem().equals("Teams"))
+            return "Notificação via Teams";
+        else
+            return proximo(pedido);
+    }
 }
