666fe51a1b352032e789c519648b9aeee0221d3c
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 11:42:11 -0300

    Criando atributo privado do tipo String tipoMensagem e criando seus getters e setters

--- Codigos/src/com/semanaquatro/chain/Request.java
+++ Codigos/src/com/semanaquatro/chain/Request.java
@@ -1,4 +1,18 @@
 package com.semanaquatro.chain;
 
 public class Request {
+
+    private String tipoMensagem;
+
+    public Request(String tipoMensagem) {
+        this.tipoMensagem = tipoMensagem;
+    }
+
+    public String getTipoMensagem() {
+        return tipoMensagem;
+    }
+
+    public void setTipoMensagem(String tipoMensagem) {
+        this.tipoMensagem = tipoMensagem;
+    }
 }
