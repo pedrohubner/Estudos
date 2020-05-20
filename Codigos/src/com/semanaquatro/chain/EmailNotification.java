f4a9140625c78c20f1266c9ed0015d13493398b7
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-20 11:44:39 -0300

    Estendendo HandlerNotificacao e implementando seu método escolher

--- Codigos/src/com/semanaquatro/chain/EmailNotification.java
+++ Codigos/src/com/semanaquatro/chain/EmailNotification.java
@@ -1,4 +1,16 @@
 package com.semanaquatro.chain;
 
-public class EmailNotification {
+public class EmailNotification extends HandlerNotificacao {
+
+    public EmailNotification(HandlerNotificacao msg) {
+        super(msg);
+    }
+
+    @Override
+    protected String escolher(Request pedido) {
+        if (pedido.getTipoMensagem().equals("Email"))
+            return "Notificação via email";
+        else
+            return proximo(pedido);
+    }
 }
