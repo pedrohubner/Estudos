50c049b2d4a7506c20d8ec29d60e3cc966f9fe4d
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-21 10:38:35 -0300

    Estendendo Coffee, implementando Adocante, criando atributo private gotasAdocante, e implementando métodos fazerCafe e adicionarAdocante

--- Codigos/src/com/semanaum/reboot/CafeAdocante.java
+++ Codigos/src/com/semanaum/reboot/CafeAdocante.java
@@ -1,4 +1,21 @@
-package com.semanaum.refazendo;
+package com.semanaum.reboot;
 
-public class CafeAdocante {
+public class CafeAdocante extends Coffee implements Adocante {
+
+    private int gotasAdocante;
+
+    CafeAdocante(boolean chaleira, int gotasAdocante) {
+        super(chaleira);
+        this.gotasAdocante = gotasAdocante;
+    }
+
+    @Override
+    protected String fazerCafe(int colheresCafe, double litrosAgua) {
+        return super.fazerCafe(colheresCafe, litrosAgua) + adicionarAdocante();
+    }
+
+    @Override
+    public String adicionarAdocante() {
+        return "\nE adicionar " + gotasAdocante + " gotas de adoçante.";
+    }
 }
