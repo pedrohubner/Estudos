32d055ae68dc6464e30bdafb375d3242a1974f08
Pedro Naa Hubner <phubner@desenvvar51-lnx.dimed.local>
2020-05-25 13:58:16 -0300

    Estendendo Additionals e implementando método makeCoffee

--- Codigos/src/com/semanaum/decorator/SugarCoffee.java
+++ Codigos/src/com/semanaum/decorator/SugarCoffee.java
@@ -1,4 +1,25 @@
 package com.semanaum.decorator;
 
-public class SugarCoffee {
+public class SugarCoffee extends Additionals {
+
+    private int sugarSpoons;
+
+    SugarCoffee(Coffee coffee, int sugarSpoons) {
+        super(coffee);
+        this.sugarSpoons = sugarSpoons;
+    }
+
+    public int getSugarSpoons() {
+        return sugarSpoons;
+    }
+
+    @Override
+    protected String makeCoffee(int colheresCafe, double litrosAgua) {
+        return coffee.makeCoffee(colheresCafe, litrosAgua) + addSugar();
+    }
+
+    String addSugar() {
+        return "\nDepois que o café for servido, adicionar " + sugarSpoons
+                + " colheres de açúcar.";
+    }
 }
