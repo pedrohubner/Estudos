2bccc56cae158c2b214e89c15e5caa91d67a006c
pedrohubner <pedronhubner@gmail.com>
2020-07-22 12:10:40 -0300

    Instanciando objetos de ElementA e Element. Criando duas threads que passam como parâmetro o método run por lambda expression e criando exemplo deadlock

---
+++ Codigos/src/main/java/com/threads/issues/deadlock/ResolveDeadLockTest.java
@@ -0,0 +1,33 @@
+package com.threads.issues.deadlock;
+
+public class ResolveDeadLockTest {
+    public static void main(String[] args) {
+
+        ElementA elementA = new ElementA(1);
+        ElementB elementB = new ElementB(1);
+
+        Thread t1 = new Thread(() -> {
+            synchronized (elementA) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (elementB) {
+                    System.out.println("In block 1");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (elementB) {
+                synchronized (elementA) {
+                    System.out.println("In block 2");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
