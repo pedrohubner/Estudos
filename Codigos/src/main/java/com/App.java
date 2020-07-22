761b6bea6a685458d4f3f1149b6fb81ff751acd6
pedrohubner <pedronhubner@gmail.com>
2020-07-22 12:09:06 -0300

    Deletando método getGreeting e implementação do mesmo na main

--- Codigos/src/main/java/com/App.java
+++ Codigos/src/main/java/com/App.java
@@ -1,11 +1,6 @@
 package com;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
     }
 }
