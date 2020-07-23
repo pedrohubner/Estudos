135814f82f6558b911ce2d05e74ea0fbda6406bd
pedrohubner <pedronhubner@gmail.com>
2020-07-23 11:17:27 -0300

    Implementando atributo String pessoaNaFila

---
+++ Codigos/src/main/java/com/datastructure/list/linkedlist/Fila.java
@@ -0,0 +1,15 @@
+package com.datastructure.list.linkedlist;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fila {
+
+    private String pessoaNaFila;
+}
