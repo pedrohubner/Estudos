e9f06fad7b12bb3df42cf6cb51b2c6de59fd9a9c
pedrohubner <pedronhubner@gmail.com>
2020-07-28 14:21:27 -0300

    Implementando exemplo de comparable

--- Codigos/src/main/java/com/features/eight/Aula.java
+++ Codigos/src/main/java/com/features/eight/Aula.java
@@ -6,28 +6,13 @@ import java.util.List;
 public class Aula {
     public static void main(String[] args) {
 
-        Aluno a1 = new Aluno("João", 63185249L, 218, 8);
-        Aluno a2 = new Aluno("Luciane", 3854752L, 202, 7);
-        Aluno a3 = new Aluno("Luana", 14578238L, 101, 7);
-        Aluno a4 = new Aluno("Carl", 2447523L, 301, 6);
+        Conselho notaFinal = (a, b) ->  a * b;
 
         List<Aluno> listaAluno = new ArrayList<>();
 
-        listaAluno.add(a1);
-        listaAluno.add(a2);
-        listaAluno.add(a3);
-        listaAluno.add(a4);
-
-        System.out.println("\nExibindo matrícula do aluno e seu nome:");
-        listaAluno.stream()
-                .map(a -> a.getMatricula() + " - " + a.getNome())
-                .forEach(System.out::println);
-
-        System.out.println("\nVerificando dados dos alunos: ");
-        listaAluno.forEach(a -> System.out.println(a.verificarAprovacao()));
-
-        System.out.println("\nVerificando alunos aprovados:");
-        listaAluno.forEach(Aluno::exibeDados);
-
+        listaAluno.add(new Aluno("Predo", 0));
+        listaAluno.add(new Aluno("Luquinhas", 0));
+        listaAluno.add(new Aluno("Irineu", 0));
+        listaAluno.add(new Aluno("Douglas", 0));
     }
 }
