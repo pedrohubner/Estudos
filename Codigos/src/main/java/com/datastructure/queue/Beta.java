d62144df45342430d202a3b34eb75b0a6f4b3719
pedrohubner <pedronhubner@gmail.com>
2020-07-28 14:26:01 -0300

    Instanciando objeto do tipo Queue

--- Codigos/src/main/java/com/datastructure/queue/Beta.java
+++ Codigos/src/main/java/com/datastructure/queue/Beta.java
@@ -1,4 +1,23 @@
 package com.datastructure.queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Beta {
+    public static void main(String[] args) {
+
+        Queue<Integer> integerQueue = new LinkedList<>();
+
+        integerQueue.add(1);
+        integerQueue.add(2);
+        integerQueue.add(3);
+        integerQueue.add(4);
+        integerQueue.add(5);
+
+        System.out.println(integerQueue);
+
+        integerQueue.poll();
+
+        System.out.println(integerQueue);
+    }
 }
