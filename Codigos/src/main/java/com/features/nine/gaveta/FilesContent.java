1286b9b10568a97e7bc05f6d5741a12cea86edd7
pedrohubner <pedronhubner@gmail.com>
2020-07-30 15:18:29 -0300

    Alterando nome da classe e deletando uso do método close()

--- Codigos/src/main/java/com/features/nine/gaveta/FilesContent.java
+++ Codigos/src/main/java/com/features/nine/gaveta/FilesContent.java
@@ -2,11 +2,10 @@ package com.features.nine.gaveta;
 
 import java.io.*;
 
-public class FilesManager {
+public class FilesContent {
 
     void readFileContent() {
         try (BufferedReader reader1 = new BufferedReader(new FileReader("greeting.txt"))) {
-            reader1.close();
             System.out.println(reader1.readLine());
         } catch (IOException e) {
             System.out.println("Deu bad");
