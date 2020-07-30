6a11b8d62ec9cd07c2e3cfe83c6ebbef74f89a59
pedrohubner <pedronhubner@gmail.com>
2020-07-30 15:18:09 -0300

    Alterando nome da classe instanciada do objeto de FilesManager para FilesContent

--- Codigos/src/main/java/com/features/nine/gaveta/Files.java
+++ Codigos/src/main/java/com/features/nine/gaveta/Files.java
@@ -3,7 +3,7 @@ package com.features.nine.gaveta;
 public class Files {
     public static void main(String[] args) {
 
-        FilesManager g1 = new FilesManager();
+        FilesContent g1 = new FilesContent();
 
         g1.readFileContent();
     }
