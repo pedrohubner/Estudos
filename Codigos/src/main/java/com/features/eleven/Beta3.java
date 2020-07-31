051c198aa21e6169b417eea3e80b1befc4c220e1
pedrohubner <pedronhubner@gmail.com>
2020-07-31 12:10:01 -0300

    Alterando exemplo

--- Codigos/src/main/java/com/features/eleven/Beta3.java
+++ Codigos/src/main/java/com/features/eleven/Beta3.java
@@ -1,35 +1,31 @@
 package com.features.eleven;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class Beta3 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-//        String endereco = " Casmurro ";
-//
-//        if (endereco.isBlank()) {
-//            System.out.println("String está vazia");
-//        }
-//        System.out.println("String tem corpo");
-//
-//        System.out.print("\nDom");
-//        System.out.print(endereco.strip());
-//        System.out.println("55");
-//
-//        System.out.print("\nDom");
-//        System.out.print(endereco.stripTrailing());
-//        System.out.println("55");
-//
-//        System.out.print("\nDom");
-//        System.out.print(endereco.stripLeading());
-//        System.out.println("55");
+        String endereco = " Casmurro ";
 
-        Path path = Files.writeString(Files.createTempFile("ppp", null),
-                "Conteúdo file" );
-        System.out.println(path);
-        String s = Files.readString(path);
-        System.out.println(s);
+        if (endereco.isBlank()) {
+            System.out.println("String está vazia");
+        }
+        System.out.println("String tem corpo");
+
+        System.out.print("\nDom");
+        System.out.print(endereco.strip());
+        System.out.println("55");
+
+        System.out.print("\nDom");
+        System.out.print(endereco.stripTrailing());
+        System.out.println("55");
+
+        System.out.print("\nDom");
+        System.out.print(endereco.stripLeading());
+        System.out.println("55");
+
+//        Path path = Files.writeString(Files.createTempFile("ppp", null),
+//                "Conteúdo file" );
+//        System.out.println(path);
+//        String s = Files.readString(path);
+//        System.out.println(s);
     }
 }
