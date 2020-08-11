398fc89927614e12aea8b6804df08222046a8a65
pedrohubner <pedronhubner@gmail.com>
2020-08-11 10:16:39 -0300

    Alterando mensagens escritas ao utilizar método setMessage do objeto message

--- Codigos/src/main/java/com/designpatterns/memento/Main.java
+++ Codigos/src/main/java/com/designpatterns/memento/Main.java
@@ -11,19 +11,19 @@ public class Main {
         Message message = new Message();
         Changes change = new Changes(backUpList);
 
-        message.setMessage("Hello, World");
+        message.setMessage("BackUp realizado em 01/08/20 às 22:05");
         change.saveChanges(message.saveBackUp());
 
-        message.setMessage("How u doing?");
+        message.setMessage("BackUp realizado em 05/08/20 às 06:32");
         change.saveChanges(message.saveBackUp());
 
-        message.setMessage("Good bye!");
-        System.out.println("Mensagem atual: " + message);
+        message.setMessage("BackUp realizado em 09/08/20 às 02:40");
+        System.out.println("Backup atual: " + message);
 
         message.getBackUp(backUpList.get(0));
-        System.out.println("Retornando primeiro backup: " + message);
+        System.out.println("Restaurando primeiro backup: " + message);
 
         message.getBackUp(backUpList.get(1));
-        System.out.println("Retornando segundo backup: " + message);
+        System.out.println("Restaurando segundo backup: " + message);
     }
 }
