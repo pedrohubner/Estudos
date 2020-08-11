84320a4bfa44c7e715d9303a547e5c1d1608ffc7
pedrohubner <pedronhubner@gmail.com>
2020-08-11 15:10:02 -0300

    Alterando tipo de dados dos atributos primitivos para wrapper e indentando classe.

--- Codigos/src/main/java/com/designpatterns/strategy/CartaoCredito.java
+++ Codigos/src/main/java/com/designpatterns/strategy/CartaoCredito.java
@@ -2,34 +2,33 @@ package com.designpatterns.strategy;
 
 public class CartaoCredito extends Opcoes {
 
-    private int parcelas;
-    private int numeroCartao;
+    private Integer parcelas;
+    private Integer numeroCartao;
     private String bandeira;
 
-    CartaoCredito(double valor, String destinatario, int parcelas, int numeroCartao, String bandeira) {
+    CartaoCredito(Double valor, String destinatario, Integer parcelas, Integer numeroCartao, String bandeira) {
         super(valor, destinatario);
         this.parcelas = parcelas;
         this.numeroCartao = numeroCartao;
         this.bandeira = bandeira;
     }
 
-    private String quantidadeParcelas(){
+    private String quantidadeParcelas() {
         if (parcelas <= 10 && parcelas != 0)
             return "Contexto parcelado em " + parcelas + "x. Valor mensal: " + (getValor() / parcelas);
-        else if (parcelas >= 11 && parcelas <= 12){
+        else if (parcelas >= 11 && parcelas <= 12) {
             return "Contexto parcelado em " + parcelas + "x. Valor mensal com juros: " +
                     (getValor() / parcelas + 10.0);
-        }
-        else
+        } else
             return "Opcção não disponível";
     }
 
     @Override
     protected String notaFiscal() {
         return "====Cartão Crédito===="
-        + "\nNúmero cartão: " + numeroCartao
-        + "\nBandeira: " + bandeira
-        + "\n" + super.notaFiscal()
-        + "\n" + quantidadeParcelas();
+                + "\nNúmero cartão: " + numeroCartao
+                + "\nBandeira: " + bandeira
+                + "\n" + super.notaFiscal()
+                + "\n" + quantidadeParcelas();
     }
 }
