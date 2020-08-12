59f513b439d9e8711a3c26bdbe1be04510c0ed65
pedrohubner <pedronhubner@gmail.com>
2020-08-12 16:50:04 -0300

    Deletando classes

--- Codigos/src/main/java/com/designpatterns/abstractfactory/fabrica/FabricaControle.java
+++
@@ -1,8 +0,0 @@
-package com.designpatterns.abstractfactory.fabrica;
-
-import com.designpatterns.abstractfactory.projeto.Controle;
-
-public interface FabricaControle {
-
-    Controle criarControle();
-}
