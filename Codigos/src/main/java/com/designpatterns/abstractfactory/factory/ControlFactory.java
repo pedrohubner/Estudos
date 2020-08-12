55774896055885ec2c1e168ddc49e9973f5d9ab5
pedrohubner <pedronhubner@gmail.com>
2020-08-12 16:52:25 -0300

    Criando método abstrato do tipo Control createControl

---
+++ Codigos/src/main/java/com/designpatterns/abstractfactory/factory/ControlFactory.java
@@ -0,0 +1,8 @@
+package com.designpatterns.abstractfactory.factory;
+
+import com.designpatterns.abstractfactory.project.Control;
+
+public interface ControlFactory {
+
+    Control createControl();
+}
