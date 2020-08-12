c7b3bbdb718cd557c2d1970a1993aff28547ed7f
pedrohubner <pedronhubner@gmail.com>
2020-08-12 11:10:29 -0300

    Instanciando um singleton lazy

--- Codigos/src/main/java/com/designpatterns/singleton/lazyload/Main.java
+++ Codigos/src/main/java/com/designpatterns/singleton/lazyload/Main.java
@@ -3,5 +3,9 @@ package com.designpatterns.singleton.lazyload;
 public class Main {
     public static void main(String[] args) {
 
+        Bean bean = Bean.getInstance();
+        Bean bean1 = Bean.getInstance();
+
+        System.out.println(bean.hashCode());
     }
 }
