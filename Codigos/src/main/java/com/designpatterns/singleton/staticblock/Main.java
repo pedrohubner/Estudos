c93530f671ff8efc279c84d81dab50df75aa80c7
pedrohubner <pedronhubner@gmail.com>
2020-08-12 11:09:48 -0300

    Instanciando objeto singleton através de um bloco estático

--- Codigos/src/main/java/com/designpatterns/singleton/staticblock/Main.java
+++ Codigos/src/main/java/com/designpatterns/singleton/staticblock/Main.java
@@ -3,6 +3,10 @@ package com.designpatterns.singleton.staticblock;
 public class Main {
     public static void main(String[] args) {
 
+        Bean bean = Bean.getInstance();
+        Bean bean1 = Bean.getInstance();
 
+        System.out.println("HashCode do objeto Bean é: " + bean.hashCode());
+        System.out.println("HashCode do objeto Bean1 é: " + bean1.hashCode());
     }
 }
