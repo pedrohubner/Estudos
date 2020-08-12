d6127e9be826afa6c9905552452a01a2d520815d
pedrohubner <pedronhubner@gmail.com>
2020-08-12 12:15:41 -0300

    Deletando objeto bean 1 e variável bean3

--- Codigos/src/main/java/com/designpatterns/singleton/lazyload/Main.java
+++ Codigos/src/main/java/com/designpatterns/singleton/lazyload/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Bean bean = Bean.getInstance();
-        Bean bean1 = Bean.getInstance();
-
-        var bean3 = bean;
-
-        bean3 = null;
 
         System.out.println(bean.hashCode());
     }
