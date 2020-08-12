606bb2a27988706cae7b3c1cebc841363dd406d0
pedrohubner <pedronhubner@gmail.com>
2020-08-12 11:10:38 -0300

    Instanciando um singleton lazy

--- Codigos/src/main/java/com/designpatterns/singleton/lazy/Main.java
+++ Codigos/src/main/java/com/designpatterns/singleton/lazy/Main.java
@@ -1,4 +1,10 @@
 package com.designpatterns.singleton.lazy;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Bean bean = Bean.validateInstance();
+
+        System.out.println(bean);
+    }
 }
